ad661da0e4e55913cb9e610c5fec29e48d32e917
frebarcelos <96388323+frebarcelos@users.noreply.github.com>
2025-07-02 09:36:30 -0300

    feat: criado estruturas

---
+++ menu.py
@@ -0,0 +1,6 @@
+from lapras import falar_ascii
+
+falar_ascii("Olá, tudo bem ?")
+falar_ascii("Bem vindo ao IndieFlix")
+falar_ascii("O que você deseja fazer ?")
+falar_ascii("[1] Criar filme [2] Listar filmes [3] Não sei")
